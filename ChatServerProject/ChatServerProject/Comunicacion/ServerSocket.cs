6103dcbbbd2107b79324d92f720eb2a95ed01946
Diego Fernandez <diegolimon123@gmail.com>
2021-04-15 01:12:26 -0400

    agrega servidor base socket

---
+++ ChatServerProject/ChatServerProject/Comunicacion/ServerSocket.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServerProject.Comunicacion
+{
+   public class ServerSocket
+    {
+
+        private StreamReader reader;
+        private StreamWriter writer;
+        private Socket comCliente;
+        private Socket servidor;
+        private int puerto;
+
+        public ServerSocket(int puerto)
+        {
+            this.puerto = puerto;
+        }
+
+        public bool Iniciar()
+        {
+            try
+            {
+                this.servidor = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+                this.servidor.Bind(new IPEndPoint(IPAddress.Any, this.puerto));
+                this.servidor.Listen(10);
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool ObtenerCliente()
+        {
+            try
+            {
+                this.comCliente = this.servidor.Accept();
+                Stream stream = new NetworkStream(this.comCliente);
+                this.writer = new StreamWriter(stream);
+                this.reader = new StreamReader(stream);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool  Escribir(string mensaje)
+        {
+            try
+            {
+
+                this.writer.WriteLine(mensaje);
+                this.writer.Flush();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public string Leer()
+        {
+            try
+            {
+                return this.reader.ReadLine().Trim();
+            }catch(Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            this.comCliente.Close();
+        }
+
+    }
+}
---
+++ ChatServerProject/ChatServerProject/Program.cs
@@ -0,0 +1,46 @@
+﻿using ChatServerProject.Comunicacion;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Int32.Parse(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("iniciando servidor en puerto {0}",puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+            if (servidor.Iniciar())
+            {
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("esperando clientes...");
+                    if (servidor.ObtenerCliente())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("conexion establecida");
+                        Console.Write("S: Hola Mundo Cliente!");
+                        servidor.Escribir("Hola Mundo Cliente!");
+                        string mensaje = servidor.Leer();
+                        Console.WriteLine("C: {0}", mensaje);
+                        servidor.CerrarConexion();
+                        
+
+
+                    }
+                }
+            }else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("no es posible iniciar servidor");
+                Console.ReadKey();
+            }
+        }
+    }
+}
